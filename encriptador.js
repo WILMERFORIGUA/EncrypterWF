d765d99efaa1cec97f5badb7e89979f8713bbf58
will <wilmerforigua@gmail.com>
2023-01-13 19:26:54 -0500

    coding logica de botones y centrando todo

--- encriptador.js
+++ encriptador.js
@@ -1,17 +1,17 @@
 function encriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted; 
-    var replacementResult = encryptedResult.replaceAll("a","ai").replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat");
+    var replacementResult = encryptedResult.replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat").replaceAll("a","ai");
     document.getElementById("respuesta").innerHTML = replacementResult;
 }
 function desencriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted;
-    var replacementResult = encryptedResult.replaceAll("ai","a").replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u");
+    var replacementResult = encryptedResult.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u").replaceAll("ai","a");
     document.getElementById("respuesta").innerHTML = replacementResult;
 }
-function copyText() {
+function copiar() {
     var copyField = document.getElementById("respuesta");  
     copyField.select();
-    document.execCommand("copy")
+    document.execCommand("copiar")
 }
