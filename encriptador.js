a13f8d3bf9708b45fb8f5120694ff085a56d7534
will <wilmerforigua@gmail.com>
2023-01-15 17:20:21 -0500

    "recuperando commit: agregando warning y alert para texto vacio"
    This reverts commit 4748320d565d2a0b536f9bcd3f1bb5c31b3099c3.

--- encriptador.js
+++ encriptador.js
@@ -1,20 +1,14 @@
 function encriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted; 
-    var replacementResult = encryptedResult.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
+    var replacementResult = encryptedResult.replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat").replaceAll("a","ai");
     document.getElementById("respuesta").innerHTML = replacementResult;
-    if (replacementResult==""){
-        alert ("ATENCIÓN!!! Introduce un texto porfavor");
-    }
 }
 function desencriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted;
-    var replacementResult = encryptedResult.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
+    var replacementResult = encryptedResult.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u").replaceAll("ai","a");
     document.getElementById("respuesta").innerHTML = replacementResult;
-    if (replacementResult==""){
-        alert ("ATENCIÓN!!! Introduce un texto porfavor");
-    }
 }
 function copiar() {
     var copyField = document.getElementById("respuesta");  
