4748320d565d2a0b536f9bcd3f1bb5c31b3099c3
will <wilmerforigua@gmail.com>
2023-01-15 04:34:10 -0500

    agregando warning y alert para texto vacio

--- encriptador.js
+++ encriptador.js
@@ -1,14 +1,20 @@
 function encriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted; 
-    var replacementResult = encryptedResult.replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("u","ufat").replaceAll("a","ai");
+    var replacementResult = encryptedResult.replaceAll("e","enter").replaceAll("i","imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");
     document.getElementById("respuesta").innerHTML = replacementResult;
+    if (replacementResult==""){
+        alert ("ATENCIÓN!!! Introduce un texto porfavor");
+    }
 }
 function desencriptar() {
     var getEncrypted = document.getElementById("mensaje").value.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
     var encryptedResult = getEncrypted;
-    var replacementResult = encryptedResult.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ufat","u").replaceAll("ai","a");
+    var replacementResult = encryptedResult.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");
     document.getElementById("respuesta").innerHTML = replacementResult;
+    if (replacementResult==""){
+        alert ("ATENCIÓN!!! Introduce un texto porfavor");
+    }
 }
 function copiar() {
     var copyField = document.getElementById("respuesta");  
